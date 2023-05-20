8cc605e9267c8aebcac853f1caa764c192a22a6f
Josip Medved <jmedved@jmedved.com>
2023-05-20 11:17:16 -0700

    Expanded document line parsing a bit

--- src/AASeq.Core/Document.cs
+++ src/AASeq.Core/Document.cs
@@ -7,9 +7,9 @@ using System.Text;
 namespace AASeq;
 
 /// <summary>
-/// AASeq document
+/// AASeq document.
 /// </summary>
-public class Document {
+public sealed record Document {
 
     /// <summary>
     /// Creates a new instance.
@@ -20,25 +20,26 @@ public class Document {
         if (stream == null) { throw new ArgumentNullException(nameof(stream), "Stream cannot be null."); }
 
         using var bStream = new BufferedStream(stream);
-        var lines = DocumentLine.Parse(bStream);
-
+        var lines = Line.GetLines(bStream);
     }
 
 
 
     [DebuggerDisplay("{Number}: {Text}")]
-    internal readonly struct DocumentLine {
+    internal readonly struct Line {
 
-        public DocumentLine(int number, byte[] bytes) {
+        public Line(int number, byte[] bytes, EolCharacter eol) {
             Number = number;
             Text = Encoding.UTF8.GetString(bytes);
+            Eol = eol;
         }
 
         public int Number { get; }
         public string Text { get; }
+        public EolCharacter Eol { get; }
 
-        public static IReadOnlyList<DocumentLine> Parse(Stream stream) {
-            var lines = new List<DocumentLine>();
+        public static IReadOnlyList<Line> GetLines(Stream stream) {
+            var lines = new List<Line>();
 
             var lineNumber = 1;
             var lineBytes = new List<byte>();
@@ -47,24 +48,24 @@ public class Document {
                 var b = stream.ReadByte();
                 if (b == -1) {  // nothing remains
                     if ((lineBytes.Count > 0) || hadCr) {
-                        lines.Add(new DocumentLine(lineNumber, lineBytes.ToArray()));
+                        lines.Add(new Line(lineNumber, lineBytes.ToArray(), hadCr ? EolCharacter.Cr : EolCharacter.None));
                     }
                     break;
                 } else if (b == 10) {
-                    lines.Add(new DocumentLine(lineNumber, lineBytes.ToArray()));
+                    lines.Add(new Line(lineNumber, lineBytes.ToArray(), hadCr ? EolCharacter.CrLf : EolCharacter.Lf));
                     lineNumber++;
                     lineBytes.Clear();
                     hadCr = false;
                 } else if (b == 13) {
                     if (hadCr) {  // two CRs in row
-                        lines.Add(new DocumentLine(lineNumber, lineBytes.ToArray()));
+                        lines.Add(new Line(lineNumber, lineBytes.ToArray(), EolCharacter.Cr));
                         lineNumber++;
                         lineBytes.Clear();
                     }
                     hadCr = true;
                 } else {
                     if (hadCr) {
-                        lines.Add(new DocumentLine(lineNumber, lineBytes.ToArray()));
+                        lines.Add(new Line(lineNumber, lineBytes.ToArray(), EolCharacter.Cr));
                         lineNumber++;
                         lineBytes.Clear();
                         hadCr = false;
@@ -77,4 +78,12 @@ public class Document {
         }
 
     }
+
+    internal enum EolCharacter {
+        None = 0,
+        Lf = 1,
+        CrLf = 2,
+        Cr = 3
+    }
+
 }
