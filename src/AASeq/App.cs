0ab01d0781179415f91b567b9a3687f241369f7b
Josip Medved <jmedved@jmedved.com>
2023-05-22 19:24:10 -0700

    Added CLI skeleton

--- src/AASeq/App.cs
+++ src/AASeq/App.cs
@@ -1,4 +1,8 @@
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.IO;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.IO;
 
 namespace AASeqCli;
@@ -6,15 +10,28 @@ namespace AASeqCli;
 internal static class App {
 
     internal static void Main(string[] args) {
-        var rootCommand = new RootCommand("Protocol simulator");
+        var versionOption = new Option<bool>(
+            aliases: new[] { "--version", "-V" },
+            description: "Show version information") {
+            Arity = ArgumentArity.Zero,
+            IsRequired = true,
+        };
 
         var verboseOption = new Option<bool>(
-            name: "--verbose",
-            description: "Each time it's specified, additional verbose output is shown") {
+            aliases: new[] { "--verbose", "-v" },
+            description: "Enable verbose output") {
             Arity = ArgumentArity.Zero,
         };
-        verboseOption.AddAlias("-v");
-        rootCommand.AddGlobalOption(verboseOption);
+
+        var rootCommand = new RootCommand("Protocol simulator") {
+            verboseOption,
+            versionOption,
+        };
+        rootCommand.SetHandler(
+            (showVersion, useVerbose) => {
+                AppExec.Base(showVersion, useVerbose);
+            },
+            versionOption, verboseOption);
 
         var fileArgument = new Argument<FileInfo>(
             name: "file",
@@ -33,26 +50,34 @@ internal static class App {
         });
         rootCommand.AddArgument(fileArgument);
 
-        var newCommand = new Command("new", "Creates a new file");
-        lintCommand.AddAlias("init");
-        newCommand.AddArgument(fileArgument);
+        var newCommand = new Command("new", "Creates a new file") {
+            verboseOption,
+            versionOption,
+            fileArgument,
+        };
         newCommand.SetHandler(AppExec.New, fileArgument);
         rootCommand.Add(newCommand);
 
-        var lintCommand = new Command("lint", "Parse file and display it");
-        lintCommand.AddAlias("show");
-        lintCommand.AddArgument(fileArgument);
+        var lintCommand = new Command("lint", "Parse file and display it") {
+            verboseOption,
+            versionOption,
+            fileArgument,
+        };
         lintCommand.SetHandler(AppExec.Lint, fileArgument);
         rootCommand.Add(lintCommand);
 
-        var runCommand = new Command("run", "Execute flows in the file");
-        runCommand.AddArgument(fileArgument);
+        var runCommand = new Command("run", "Execute flows in the file") {
+            verboseOption,
+            versionOption,
+            fileArgument,
+        };
         runCommand.SetHandler(AppExec.Run, fileArgument);
         rootCommand.Add(runCommand);
 
-        rootCommand.SetHandler(AppExec.Run, fileArgument);
-
-        rootCommand.Invoke(args);
+        var cliBuilder = new CommandLineBuilder(rootCommand);
+        cliBuilder.UseHelp();
+        var cliParser = cliBuilder.Build();
+        cliParser.Invoke(args, new SystemConsole());
     }
 
 }
--- src/AASeq/AppExec.cs
+++ src/AASeq/AppExec.cs
@@ -1,9 +1,24 @@
 using AASeq;
+using System;
 using System.IO;
+using System.Reflection;
 
 namespace AASeqCli;
 
 internal static class AppExec {
+
+    public static void Base(bool showVersion, bool useVerbose) {
+        if (showVersion) {
+            var assembly = Assembly.GetEntryAssembly();
+            var name = assembly?.GetName().Name ?? string.Empty;
+            var version = assembly?.GetName().Version ?? new Version();
+            Console.WriteLine($"{name} {version.Major}.{version.Minor} {version.Revision}");
+            if (useVerbose) {
+            }
+            return;
+        }
+    }
+
 
     public static void New(FileInfo file) {
         using var stream = file.OpenRead();
