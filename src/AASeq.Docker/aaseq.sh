61c2ed225814300875195dfef7147ead8ee31b6d
Josip Medved <jmedved@jmedved.com>
2025-04-16 23:10:17 -0700

    Add docker files

---
+++ src/AASeq.Docker/aaseq.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+"$SCRIPT_DIR/bin/AASeq" "$@"
---
+++ src/AASeq.Docker/bash-default.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias ll='ls -la'
+PATH=$PATH:/app
+
+PS1='\n\u@\h \w\n\$ '
