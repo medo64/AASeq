2698105cc6a16b8bbc80a89eb002607bbd979784
Josip Medved <jmedved@jmedved.com>
2025-05-05 19:46:57 -0700

    Updating ping to include warnings

--- src/AASeq.Plugin.Ping/Ping.cs
+++ src/AASeq.Plugin.Ping/Ping.cs
@@ -17,21 +17,58 @@ internal sealed class Ping : IEndpointPlugin {
     /// Gets the instance.
     /// </summary>
     public static IEndpointPlugin CreateInstance(ILogger logger, AASeqNodes configuration) {
-        return new Ping(configuration);
+        return new Ping(logger, configuration);
     }
 
 
-    private Ping(AASeqNodes configuration) {
-        DontFragment = configuration["DontFragment"].AsBoolean(DefaultDontFragment);
-        Host = configuration["Host"].AsString(DefaultHost);
-        Timeout = configuration["Timeout"].AsTimeSpan(DefaultTimeout);
-        TimeToLive = configuration["TTL"].AsInt32(configuration["TimeToLive"].AsInt32(DefaultTTL));
+    private Ping(ILogger logger, AASeqNodes configuration) {
+        Logger = logger;
+
+        if (configuration.TryConsumeNode("DontFragment", out var dfNode)) {
+            if (dfNode.Properties.Count > 0) { logger.LogWarning($"Unrecognized properties on '{dfNode.Name}'."); }
+            var dfValue = dfNode.Value.AsBoolean();
+            if (dfValue != null) {
+                DontFragment = dfValue.Value;
+            } else {
+                logger.LogWarning($"Cannot convert 'DontFragment' value.");
+            }
+        }
+        if (configuration.TryConsumeNode("Host", out var hostNode)) {
+            if (hostNode.Properties.Count > 0) { logger.LogWarning($"Unrecognized properties on '{hostNode.Name}'."); }
+            var hostValue = hostNode.Value.AsString();
+            if (!string.IsNullOrWhiteSpace(hostValue)) {
+                Host = hostValue;
+            } else {
+                logger.LogWarning($"Cannot convert 'Host' value.");
+            }
+        }
+        if (configuration.TryConsumeNode("Timeout", out var timeoutNode)) {
+            if (timeoutNode.Properties.Count > 0) { logger.LogWarning($"Unrecognized properties on '{timeoutNode.Name}'."); }
+            var timeoutValue = timeoutNode.Value.AsTimeSpan();
+            if ((timeoutValue != null) && (timeoutValue.Value.TotalSeconds > 0)) {
+                Timeout = timeoutValue.Value;
+            } else {
+                logger.LogWarning($"Cannot convert 'Timeout' value.");
+            }
+        }
+        if (configuration.TryConsumeNode("TTL", out var ttlNode)) {
+            if (ttlNode.Properties.Count > 0) { logger.LogWarning($"Unrecognized properties on '{ttlNode.Name}'."); }
+            var ttlValue = ttlNode.Value.AsInt32();
+            if ((ttlValue != null) && (ttlValue.Value is > 0 and < 255)) {
+                TimeToLive = ttlValue.Value;
+            } else {
+                logger.LogWarning($"Cannot convert 'TTL' value.");
+            }
+        }
+
+        if (configuration.Count > 0) { logger.LogWarning($"Unrecognized configuration node '{configuration[0].Name}'."); }
     }
 
-    private readonly bool DontFragment;
-    private readonly string Host;
-    private readonly TimeSpan Timeout;
-    private readonly int TimeToLive;
+    private readonly ILogger Logger;
+    private readonly bool DontFragment = false;
+    private readonly string Host = "localhost";
+    private readonly TimeSpan Timeout = TimeSpan.FromSeconds(1);
+    private readonly int TimeToLive = 64;
 
     private readonly ConcurrentDictionary<Guid, (string, AASeqNodes)> Storage = [];
 
@@ -52,12 +89,46 @@ internal sealed class Ping : IEndpointPlugin {
     /// <param name="parameters">Parameters.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     public async Task<AASeqNodes> SendAsync(Guid id, string messageName, AASeqNodes parameters, CancellationToken cancellationToken) {
-        var pingOptions = new PingOptions {
-            DontFragment = parameters["DontFragment"].AsBoolean(DontFragment),
-            Ttl = parameters["TTL"].AsInt32(TimeToLive),
-        };
+        var dontFragment = DontFragment;
+        if (parameters.TryConsumeNode("DontFragment", out var dfNode)) {
+            if (dfNode.Properties.Count > 0) { Logger.LogWarning($"Unrecognized properties on '{dfNode.Name}'."); }
+            var dfValue = dfNode.Value.AsBoolean();
+            if (dfValue != null) {
+                dontFragment = dfValue.Value;
+            } else {
+                Logger.LogWarning($"Cannot convert 'DontFragment' value.");
+            }
+        }
 
-        var timeout = parameters["Timeout"].AsTimeSpan(Timeout);
+        var timeout = Timeout;
+        if (parameters.TryConsumeNode("Timeout", out var timeoutNode)) {
+            if (timeoutNode.Properties.Count > 0) { Logger.LogWarning($"Unrecognized properties on '{timeoutNode.Name}'."); }
+            var timeoutValue = timeoutNode.Value.AsTimeSpan();
+            if ((timeoutValue != null) && (timeoutValue.Value.TotalSeconds > 0)) {
+                timeout = timeoutValue.Value;
+            } else {
+                Logger.LogWarning($"Cannot convert 'Timeout' value.");
+            }
+        }
+
+        var ttl = TimeToLive;
+        if (parameters.TryConsumeNode("TTL", out var ttlNode)) {
+            if (ttlNode.Properties.Count > 0) { Logger.LogWarning($"Unrecognized properties on '{ttlNode.Name}'."); }
+            var ttlValue = ttlNode.Value.AsInt32();
+            if ((ttlValue != null) && (ttlValue.Value is > 0 and < 255)) {
+                ttl = ttlValue.Value;
+            } else {
+                Logger.LogWarning($"Cannot convert 'TTL' value.");
+            }
+        }
+
+        if (parameters.Count > 0) { Logger.LogWarning($"Unrecognized node '{parameters[0].Name}'."); }
+
+
+        var pingOptions = new PingOptions {
+            DontFragment = dontFragment,
+            Ttl = ttl,
+        };
 
 #pragma warning disable CS4014
         Task.Run(() => {
@@ -75,11 +146,12 @@ internal sealed class Ping : IEndpointPlugin {
 #pragma warning restore CS4014
 
         await Task.CompletedTask.ConfigureAwait(false);
-        return new AASeqNodes {
-            new AASeqNode("DontFragment", pingOptions.DontFragment),
-            new AASeqNode("TTL", pingOptions.Ttl),
-            new AASeqNode("Timeout", timeout),
-        };
+
+        var result = new AASeqNodes();
+        if (pingOptions.DontFragment != DontFragment) { result.Add(new AASeqNode("DontFragment", pingOptions.DontFragment)); }
+        if (pingOptions.Ttl != TimeToLive) { result.Add(new AASeqNode("TTL", pingOptions.Ttl)); }
+        if (timeout != Timeout) { result.Add(new AASeqNode("Timeout", timeout)); }
+        return result;
     }
 
     /// <summary>
@@ -101,11 +173,5 @@ internal sealed class Ping : IEndpointPlugin {
         }
         throw new InvalidOperationException("No reply.");
     }
-
-
-    private const bool DefaultDontFragment = false;
-    private const string DefaultHost = "localhost";
-    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(1);
-    private const int DefaultTTL = 64;
 
 }
